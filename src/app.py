5902ba54a3872b06e6449295459fd2f4ffefe973
seigneurfuo <seigneurfuo@protonmail.com>
2024-06-25 23:49:56 +0200

    Commit de diverses modifications jamais commitées

--- src/app.py
+++ src/app.py
@@ -1,11 +1,13 @@
 import json
 import os.path
 import sys
+import webbrowser
 from pathlib import Path
+from pprint import pprint
 
 from PyQt6.QtCore import QTimer
 from PyQt6.QtGui import QIcon
-from PyQt6.QtWidgets import QSystemTrayIcon, QMenu, QApplication
+from PyQt6.QtWidgets import QSystemTrayIcon, QMenu, QApplication, QDialog
 
 import requests
 
@@ -13,19 +15,24 @@ class StatusIcon(QSystemTrayIcon):
     def __init__(self):
         super().__init__()
 
+        self.connection_error_count = 0
+
         self.actions = []
 
         self.load_config()
 
         self.setIcon(QIcon.fromTheme("home"))
 
+
         # QTimer
         self.timer = QTimer()
         self.timer.timeout.connect(self.tick)
-        self.timer.start()
         self.timer.setInterval(self.config["update_interval"])
+        self.timer.start()
 
         self.init_ui()
+
+        self.tick()
 
     def load_config(self):
         with open(os.path.join(Path.home(), ".config", "home-assistant-indicator.json")) as config_file:
@@ -41,6 +48,9 @@ class StatusIcon(QSystemTrayIcon):
 
         self.menu.addSeparator()
 
+        open_webbrowser = self.menu.addAction(QIcon.fromTheme("web"), "Ouvrir dans le navigateur")
+        open_webbrowser.triggered.connect(self.open_webbrowser)
+
         close_action = self.menu.addAction(QIcon.fromTheme("close"), "Quitter")
         close_action.triggered.connect(self.close)
 
@@ -48,32 +58,70 @@ class StatusIcon(QSystemTrayIcon):
 
     def tick(self):
         for action_index, sensor in enumerate(self.config["sensors"]):
-            name, state = self.get_sensor_state(sensor)
+            sensor_data = self.get_sensor_data(sensor)
+
+            if self.connection_error_count == 3:
+                self.connection_error_count = 0
+
+                title = ""
+                msg = ""
+                self.showMessage(title, msg)
+
+            # Erreur
+            if not sensor_data:
+                return
+
+            name = sensor_data["attributes"]["friendly_name"]
+            state = sensor_data["state"]
+            unit = sensor_data["attributes"]["unit_of_measurement"]
+
             msg = f"{name}: {state}"
 
-            # Permet d'afficher l'information du premier capteur
+            if self.config["show_units"]:
+                msg += unit
+
+            # Permet d'afficher l'information du premier capteur dans le tooltip
             if action_index == 0:
-                self.setToolTip(str(state))
+                self.setToolTip(msg)
 
             self.actions[action_index].setText(msg)
 
-    def get_sensor_state(self, entity_id):
-        url = f"{self.config['api_entrypoint']}/states/{entity_id}"
-        headers = {
-            "Authorization": f"Bearer {self.config['key']}",
-            "content-type": "application/json",
-        }
+    def get_sensor_data(self, entity_id):
+        try:
+            url = f"{self.config['url']}/api/states/{entity_id}"
+            headers = {
+                "Authorization": f"Bearer {self.config['key']}",
+                "content-type": "application/json",
+            }
 
-        response = requests.get(url, headers=headers)
-        json_response = response.json()
-        name = json_response["attributes"]["friendly_name"]
-        state = json_response["state"]
+            response = requests.get(url, headers=headers)
+            return response.json()
 
-        return name, state
+        except:
+            return None
+
+    def open_webbrowser(self):
+        webbrowser.open_new_tab(self.config["url"])
 
     def close(self):
         self.setVisible(False)
         exit(0)
+
+class SettingsWindow(QDialog):
+    def __init__(self, parent):
+        super(self).__init__()
+
+        self.parent = parent
+
+        self.init_ui()
+        self.init_events()
+
+    def init_events(self):
+        pass
+
+    def init_ui(self):
+        pass
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
